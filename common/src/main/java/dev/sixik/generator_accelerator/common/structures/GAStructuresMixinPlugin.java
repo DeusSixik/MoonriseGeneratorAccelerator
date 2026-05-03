d0ddfe02ae323fceee493fff30c47273c75f130a
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-05-03 20:15:22 +0300

    feat: Add compat with mod Zeta

--- common/src/main/java/dev/sixik/generator_accelerator/common/structures/GAStructuresMixinPlugin.java
+++ common/src/main/java/dev/sixik/generator_accelerator/common/structures/GAStructuresMixinPlugin.java
@@ -1,6 +1,7 @@
 package dev.sixik.generator_accelerator.common.structures;
 
 import dev.sixik.generator_accelerator.api.mixin.GAMixinPlugin;
+import dev.sixik.generator_accelerator.api.mixin.MixinApplier;
 import dev.sixik.generator_accelerator.config.GAConfig;
 
 public class GAStructuresMixinPlugin extends GAMixinPlugin {
@@ -11,6 +12,9 @@ public class GAStructuresMixinPlugin extends GAMixinPlugin {
 
     @Override
     public void onLoad(String s) {
-
+        create("org.violetmoon.zeta.Zeta", new MixinApplier.Param(
+                "dev.sixik.generator_accelerator.common.structures.mixin.compats.zeta.Zeta$StructurePiece$Fix",
+                "org.violetmoon.zeta.mixin.mixins.StructurePieceMixin"
+        ));
     }
 }
---
+++ fabric/src/main/java/dev/sixik/generator_accelerator/common/structures/mixin/compats/zeta/Zeta$StructurePiece$Fix.java
@@ -0,0 +1,8 @@
+package dev.sixik.generator_accelerator.common.structures.mixin.compats.zeta;
+
+import net.minecraft.world.level.levelgen.structure.StructurePiece;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(StructurePiece.class)
+public class Zeta$StructurePiece$Fix {
+}
