9c79ada10e5c369f7d95fe4572c664e814d20162
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-05-11 02:30:53 +0300

    feat: Better synchronization

--- common/src/main/java/dev/sixik/generator_accelerator/common/structures/GAStructuresMixinPlugin.java
+++ common/src/main/java/dev/sixik/generator_accelerator/common/structures/GAStructuresMixinPlugin.java
@@ -25,7 +25,8 @@ public class GAStructuresMixinPlugin extends GAMixinPlugin {
 
         create(GeneratorAccelerator.C2ME_MOD, new MixinApplier.Param("",
                 "com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading.MixinStructure",
-                "com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading.MixinStructurePalettedBlockInfoList"
+                "com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading.MixinStructurePalettedBlockInfoList",
+                "com.ishland.c2me.fixes.worldgen.threading_issues.mixin.threading.MixinStructureChecker"
         ));
     }
 }
