eb33705eefe9da04dd7499e7ded8e0bb5b0998ea
Bogdan <142807122+DeusSixik@users.noreply.github.com>
2026-06-26 00:02:19 +0300

    and some delete

--- fabric/src/main/java/dev/sixik/generator_accelerator/common/features/mixin/compats/alexscaves/AlexsCaves$ChunkGeneratorMixin$applyBiomeDecoration.java
+++
@@ -1,8 +0,0 @@
-package dev.sixik.generator_accelerator.common.features.mixin.compats.alexscaves;
-
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ChunkGenerator.class)
-public class AlexsCaves$ChunkGeneratorMixin$applyBiomeDecoration {
-}
--- fabric/src/main/java/dev/sixik/generator_accelerator/common/features/mixin/compats/confluence/Confluence$FeaturePlacementCompatMixin.java
+++
@@ -1,8 +0,0 @@
-package dev.sixik.generator_accelerator.common.features.mixin.compats.confluence;
-
-import dev.sixik.generator_accelerator.common.features.vm.FeaturePlacementCompat;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(FeaturePlacementCompat.class)
-public class Confluence$FeaturePlacementCompatMixin {
-}
